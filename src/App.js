1282b44c405032d86fcfecc4d91c76623016dbe4
Sunil Karki <karkisunil1200@gmail.com>
2019-07-25 13:25:27 +0545

    Starting Project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {createStore} from 'redux';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
@@ -11,15 +12,14 @@ const App = () => {
     car: {
       price: 26395,
       name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      image: 'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
       features: []
     },
     store: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
+      {id: 1, name: 'V-6 engine', price: 1500},
+      {id: 2, name: 'Racing detail package', price: 1500},
+      {id: 3, name: 'Premium sound system', price: 500},
+      {id: 4, name: 'Rear spoiler', price: 250}
     ]
   };
 
@@ -32,12 +32,12 @@ const App = () => {
   };
 
   return (
-    <div className="boxes">
-      <div className="box">
+    <div className='boxes'>
+      <div className='box'>
         <Header car={state.car} />
         <AddedFeatures car={state.car} />
       </div>
-      <div className="box">
+      <div className='box'>
         <AdditionalFeatures store={state.store} />
         <Total car={state.car} additionalPrice={state.additionalPrice} />
       </div>
